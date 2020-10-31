4f97049088f19b0d453718840b8121bceb22d6db
nakatievent <nakatievent@gmail.com>
2020-10-31 09:03:15 +0900

    session12の実装

---
+++ session12.js
@@ -0,0 +1,62 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello,Vue.js!"
+  }
+})
+
+var app2 = new Vue({
+  el: "#app2",
+  data: {
+    message: "Hello,Vue.js!"
+  }
+})
+
+var app3 = new Vue({
+  el: "#app3",
+  data: {
+    checked: false
+  }
+})
+
+var app4 = new Vue({
+  el: "#app4",
+  data: {
+    colors: []
+  }
+})
+
+var app5 = new Vue({
+  el: "#app5",
+  data: {
+    colors: ''
+  }
+})
+
+var app6 = new Vue({
+  el: "#app6",
+  data: {
+    selected: []
+  }
+})
+
+var app7 = new Vue({
+  el: "#app7",
+  data: {
+    message: ''
+  }
+})
+
+var app8 = new Vue({
+  el: "#app8",
+  data: {
+    message: ''
+  }
+})
+
+var app9 = new Vue({
+  el: "#app9",
+  data: {
+    age: 0
+  }
+})
